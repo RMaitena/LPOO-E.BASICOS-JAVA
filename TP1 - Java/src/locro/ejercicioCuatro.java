a5b885a522b111cdc88dd8f81ea8d11526c8cb37
RMaitena <130392275+RMaitena@users.noreply.github.com>
2024-05-28 18:27:44 -0300

    asd

---
+++ TP1 - Java/src/locro/ejercicioCuatro.java
@@ -0,0 +1,15 @@
+package locro;
+
+public class ejercicioCuatro {
+    public static void main(String[] args) {
+        System.out.println("¡Ejercicio 4! :)");
+        
+        String[] nombres = {"Mai", "Lu", "Nini"};
+
+        System.out.println("Lista de nombres:");
+        
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
